2126d405f24d010d63c0bcedafe82886f943b2d3
malkitsingh1234 <malkitsingh@zatest.biz>
2025-09-28 14:38:03 +0530

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const API_BASE = 'http://localhost:8000/api/';
+const API_BASE = 'https://9fe5de3bb5ca.ngrok-free.app/api/';
 
 interface Email {
   id: number;
